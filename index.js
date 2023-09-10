89862ebe7b58ca523b63a829b4eefa79ce588cd8
AlokKumarRai <alok20530@iiitkalyani.ac.in>
2023-09-11 00:43:42 +0530

    cors added

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-const connectToMongo = require('./db/connection');
+const connectToMongo = require("./db/connection");
 
-var cors = require('cors');     
-app.use(cors());   
+// var cors = require('cors');
+// app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 app.use(express.json());
 
-// Available routes 
-app.use('/api/user',require('./routes/User'));
-app.use('/api/cart',require('./routes/Cart'));
-app.use('/api/placeorder',require('./routes/Order'));
+// Available routes
+app.use("/api/user", require("./routes/User"));
+app.use("/api/cart", require("./routes/Cart"));
+app.use("/api/placeorder", require("./routes/Order"));
 
-app.get('/',(req,res)=>{
-    res.send('hello');
-})
+app.get("/", (req, res) => {
+  res.send("hello");
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
